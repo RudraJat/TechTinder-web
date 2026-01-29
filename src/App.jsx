89ef6164fc19c660f792e4d93503af5c56522907
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2026-01-29 22:13:20 +0530

    added tailwindcss and daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Navbar from './Components/Navbar';
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <h1>Hello world</h1>
-      </div>  
+      <Navbar/>
+      <h1 className="text-4xl font-bold text-red-600">
+        Hello World
+      </h1>
     </>
-  )
+  );
 }
 
 export default App;
